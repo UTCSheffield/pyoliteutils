9b21320837ffeb28c503b1030b2de0370f238dfe
Mr Eggleton <meggleton@utcsheffield.org.uk>
2024-01-30 11:44:04 +0000

    Remove the install thing just run micropip install with the dependencies as well when installing pyoliteutils

--- pyoliteutils/__init__.py
+++ pyoliteutils/__init__.py
@@ -1,7 +1,3 @@
-async def install(): 
-    import micropip
-    await micropip.install(["matplotlib", "pandas"])
-
 from .pyoliteutils import load_file_into_in_mem_filesystem
 from .mermaid import mm
 from .lessonsurvey import lessonsurvey

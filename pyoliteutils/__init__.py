f1c21cfc349ac611761069a89d122ae71b7a9866
Mr Eggleton <meggleton@utcsheffield.org.uk>
2024-02-21 14:01:36 +0000

    remove .info

--- pyoliteutils/__init__.py
+++ pyoliteutils/__init__.py
@@ -3,7 +3,7 @@ from .mermaid import mm
 from .kroki import kroki, plantuml, ditaa, graphviz, mermaid
 from .lessonsurvey import lessonsurvey
 from .wordcloud import wordcloud
-from .info import pyoliteutilsinfo
+#from .info import pyoliteutilsinfo
 
 from . import _version
 __version__ = _version.get_versions()['version']
